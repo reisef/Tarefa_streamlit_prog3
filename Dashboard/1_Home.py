32e311868041f04f70fe528be1d72ecbf2206c87
reisef <54605305+reisef@users.noreply.github.com>
2025-11-25 21:47:09 -0300

    Delete Dashboard directory

--- Dashboard/1_Home.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Relatório Mundial da Felicidade 2024", layout="wide")
-
-st.sidebar.title("📌 Navegação")
-st.sidebar.write("Escolha uma página no menu acima.")
-
-st.title("🌍 Relatório Mundial da Felicidade 2024")
-st.write("Use o menu lateral para navegar entre as páginas.")
